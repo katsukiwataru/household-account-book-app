1091c2c8eda44ce8ebfb80c7bc7c343c1fc7a2f1
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-11-28 02:28:10 +0900

    feat: add environment

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: src + '/tsx/index.tsx',
+  output: {
+    path: dist,
+    filename: 'js/index.js',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  devServer: {
+    open: true, //ブラウザを自動で開く
+    openPage: 'index.html', //自動で指定したページを開く
+    contentBase: dist, // HTML等コンテンツのルートディレクトリ
+    watchContentBase: true, //コンテンツの変更監視をする
+    port: 3000, // ポート番号
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: 'ts-loader' }],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: ['url-loader?limit=10000', 'img-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
